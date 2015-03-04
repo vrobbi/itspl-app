6c34c01145499ab69359438501d23b64ed0c874b
vrobbi <vrobbi@libero.it>
2015-03-04 21:02:09 +0100

    added split() function

--- app1.js
+++ app1.js
@@ -1,11 +1,11 @@
 var app = require('http').createServer(handler),
 	io = require('socket.io').listen(app),
 	nstatic = require('node-static'),
-urlimageroom =[];
-var   data64 =[];
-var len1 =0;
-// This will make all the files in the current folder
-// accessible from the web
+ stringremote = '',
+fields=[],
+fetchedstring ='';
+
+
 var fileServer = new nstatic.Server('./');
 // This is the port for our web server.
 // you will need to go to http://localhost:8080 to see it
@@ -36,76 +36,38 @@ socket.on('setuproom', function (data) {
  	if (myregexp.test(data.room)=== true)   {
  socket.join(data.room);
  socket.nickname = data.usernamerem;
- console.log('new connection roomid: '+ data.room);
- if (data.imageBG ===  ''){
-var occorrenza ='';
-	 var len = urlimageroom.length;
-	for (i=len; i!=0; i--) {
-		// var lunghezzastringa = urlimageroom[i].length;
-occorrenza = urlimageroom[i-1].lastIndexOf('_');		 
-//socket.emit('setuproomser', data);
-//    console.log(urlimageroom[i-1].substr(0,occorrenza));
-if (data.room === urlimageroom[i-1].substr(occorrenza+1)){
-socket.emit('setuproomser', {
-			'room' :  data.room,
-			'imageBG' : urlimageroom[i-1].substr(0,occorrenza)				
-			});	
-break;	
-	}
-	 }
- }
- 
- for (n=0; n<len1; n++) {
-		// var lunghezzastringa = urlimageroom[i].length;
-var occorrenza640  = data64[n].indexOf('_'); 
-if (data64[n].substr(0,occorrenza640) === data.room) {
-socket.emit('setupcanvasser', {
-			'room' :  data.room,
-			'canvasstring' : data64[n].substr(occorrenza640+1)				
-			});		
-break;
- }
-}
+ console.log('new connection roomid: '+ data.room + ' ' +  data.usernamerem);
+
 }
 }); 
 
 
-socket.on('base64data', function (data) { 
-var occorrenza64 ='';	
-var insertion =false;
-var occorrenza64 = data.base64data.indexOf('_');	
-var extractroom =  data.base64data.substr(0,occorrenza64);
-len1 = data64.length;
-	// console.log(len);
-	// console.log(data.base64data.length);
-	
-	
-for (i=0; i<len1; i++) {
-occorrenza64  = data64[i].indexOf('_'); 
-if (data64[i].substr(0,occorrenza64) === extractroom ) {
-data64[i] =  data.base64data;	
-insertion = true;
-console.log('update arraycanvas ' + data64.length);
-break;
- }
+socket.on('chat', function (data) {
+stringremote = data.testochat;
+fetchedstring='';
+fields.length = 0;
+fields =  stringremote.split("/");
 
-}
-if (insertion == false) {
-data64.push(data.base64data);
-console.log	(data64.length + ' new insert') ;
-}
-}); 
+for (i=0;i<fields.length;i++) {
+fetchedstring =  fetchedstring + '   ' + 	fields[i];
+	
+	};
+fetchedstring =  'Fields parsed from server: ' + fields.length + ' = ' + fetchedstring;		
 
-	// Start listening for mouse move events
-	socket.on('mousemove', function (data) {
+		socket.broadcast.to(data.room).emit('chatser',
+		{									
+		'testochat' :	fetchedstring,
+		'id': data.id,
+		'room' : data.room										
+		});
 		
-	socket.broadcast.to(data.room).emit('moving', data);
-						
-	});
-	
-		socket.on('deletezone', function (data) {
-	socket.broadcast.to(data.room).emit('deletezoneser', data);
- });
+		socket.emit('chatresponse', {
+			'testochat' : fetchedstring,
+			'room' :  data.room,
+			'id': data.id
+			});	
+		
+	});	
 
 socket.on('loadimage', function (data) {
 //occorrenza = urlimageroom[i].lastIndexOf('_');		 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,187 +1,74 @@
 jQuery(function(){
 	// This demo depends on the canvas element
-	if(!('getContext' in document.createElement('canvas'))){
-		alert('Sorry, it looks like your browser does not support canvas!');
-		return false;
-	}     	
-	document.getElementById('pencil').style.border="2px solid orange";  
-jQuery("#divrubber").draggable({ cursor: "move" }).resizable();		
-document.getElementById('divrubber').style.width="70px";
-document.getElementById('divrubber').style.height="70px";
-document.getElementById('size1').style.border="2px solid orange";
-// document.getElementByClassName("sizepencil").style.border =""; 
 	// The URL of your web server (the port is set in app.js)
 	//var url = 'http://localhost:3000'
-	var divrubber = jQuery("#divrubber");
-	var imageBG ='';
+/*	
+	var s = 'john smith/123 Street/Apt 4';
+var fields = s.split(/\//);
+var name = fields[0];
+var street = fields[1];
+var fetchedstring ='';
+
+for (i=0;i<fields.length;i++) {
+fetchedstring =  fetchedstring + '   ' + 	fields[i];
+	
+	};
+	console.log('Fields fetched: ' + fields.length + ' = ' + fetchedstring);
+*/	
 	var color ='';
 	var username = '';
 	var roomid = '';
-	var pencilsize = 1;
-	var positionx ='23';
-	var positiony='0';
-	var string64data = '';
+	var id = Math.round(jQuery.now()*Math.random());	
 	  var url = window.location.hostname;
-	color = getParameterByName('color');
-	if (color.length == 6){  color = '#' + color};
-	
+		
 	username = getParameterByName('username');
-	if (username.length > 1 && username.length < 25){  username = getParameterByName('username')};
+	if (username.length > 1 && username.length < 25){  username = getParameterByName('username')}
+	else {
+username = id;		
+	};
+	
+	
 	
 	roomid = getParameterByName('roomID');
 	if (roomid.length > 0){  roomid = getParameterByName('roomID')};	
 	imageBG = getParameterByName('imageBG');
 	
 			
-	var doc = jQuery(document),
-		canvas = jQuery('#respondcanvas'),
-		bgcanvas = jQuery('#bgcanvas');
-//	instructions = jQuery('#instructions');
-	var ctx = canvas[0].getContext('2d');	
-	var ctx1 = bgcanvas[0].getContext('2d');	
-	var drawing = false;
-	var controlpencil = true;
-	var controlrubber = false;
-	var clients = {};
-	var cursors = {};
-	canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-	bgcanvas.width = document.body.clientWidth;
-	bgcanvas.height = document.body.clientHeight;
+	var doc = jQuery(document);
 	
+		
 var socket = io.connect(url); 
-
-// var spessore = jQuery('#spessore').value;
-var colorem;
- 
-    ctx.lineCap = 'round';
-    ctx.lineJoin = 'round';
- //   ctx.lineWidth =  2;
- ctx.font = "20px Tahoma";
  
  if (roomid.length > 0) {
 socket.emit('setuproom',{
 				'room': roomid,				
-				'usernamerem' : username,
-				'imageBG': imageBG
+				'usernamerem' : username				
 			});
 } 
 
- if (imageBG.length > 3){ 
-	imageobj = new Image();
-	imageobj.src = imageBG;
-	imageobj.onload = function() {
-		ctx1.clearRect(0,0,bgcanvas.width,bgcanvas.height);
-         ctx1.drawImage(imageobj, positionx, positiony);
-	// loadImages(sources, function(images) {
-									};
-      
-	 // alert(roomid);
-	   socket.emit('loadimage',{
-				'imageBG' : imageBG,
-			    'usernamerem' : username,
-				'room' : roomid				
-			});
+
 	
-	};	
+	jQuery('#scrivi').keypress(function(e){
+var code = e.keyCode;
+if (code == '13') {
+  if (document.getElementById('scrivi').value.length > 0 ) {	
 
-  
- $('#rubber').click(function(e) {
-document.getElementById('rubber').style.border="2px solid orange";
-document.getElementById('pencil').style.border="";
-document.getElementById('divrubber').style.display="block";
-controlrubber = true;
-controlpencil = false;
-    });	
- 
- $('#pencil').click(function(e) {
-document.getElementById('pencil').style.border="2px solid orange";
-document.getElementById('rubber').style.border="";
-controlrubber = false;
-controlpencil = true;
-document.getElementById('divrubber').style.display="none";
-    });
- 
-  $('#size1').click(function(e) {
-document.getElementById('size1').style.border="2px solid orange";
-document.getElementById('size2').style.border="";
-document.getElementById('size3').style.border="";
-document.getElementById('size4').style.border="";
-document.getElementById('size5').style.border="";
-pencilsize = 1;
-    });
-  
-    $('#size2').click(function(e) {
-document.getElementById('size2').style.border="2px solid orange";
-document.getElementById('size1').style.border="";
-document.getElementById('size3').style.border="";
-document.getElementById('size4').style.border="";
-document.getElementById('size5').style.border="";
-pencilsize = 2;
-    });
-	
-	  $('#size3').click(function(e) {
-document.getElementById('size3').style.border="2px solid orange";
-document.getElementById('size2').style.border="";
-document.getElementById('size1').style.border="";
-document.getElementById('size4').style.border="";
-document.getElementById('size5').style.border="";
-pencilsize = 7;
-    });
-	  
-	    $('#size4').click(function(e) {
-document.getElementById('size4').style.border="2px solid orange";
-document.getElementById('size2').style.border="";
-document.getElementById('size3').style.border="";
-document.getElementById('size1').style.border="";
-document.getElementById('size5').style.border="";
-pencilsize = 15;
-    });
-$('#size5').click(function(e) {
-document.getElementById('size5').style.border="2px solid orange";
-document.getElementById('size2').style.border="";
-document.getElementById('size3').style.border="";
-document.getElementById('size4').style.border="";
-document.getElementById('size1').style.border="";
-pencilsize = 25;
-    });
- 
-$('#divrubber').on('mousemove', function(e){								 
-
-// jQuery('#divrubber').dblclick(function (){
-if(document.getElementById('checkerase').checked) {
-			var posizionerubber = divrubber.position();
-			var rubberwidth =   document.getElementById('divrubber').style.width;
-			var rubberheight = document.getElementById('divrubber').style.height;
-ctx.clearRect(posizionerubber.left, posizionerubber.top, rubberwidth.substr(0,rubberwidth.length -2), rubberheight.substr(0,rubberheight.length -2));	
-
-socket.emit('deletezone',{
-				'x': posizionerubber.left,
-				'y': posizionerubber.top,
-				'width': rubberwidth.substr(0,rubberwidth.length -2),
-				'height': rubberheight.substr(0,rubberheight.length -2),
-			    'usernamerem' : username,
-				'spessremo' : pencilsize,
-				'room' : roomid				
+ socket.emit('chat',{
+				'testochat': document.getElementById('scrivi').value,				
+				'id': id,
+				'usernamerem' : username,
+				'room' : roomid
 			});
-}
-// canvas2base64();
-});
 
-$('#divrubber').on('mouseup', function(e){
-console.log ('mouseup rubber');									   
-canvas2base64();
- });
+  document.getElementById('scrivi').value ='';
 
-  socket.on('loadimageser', function (data) {
-var imgdaclient = new Image();
-//alert(data.imageBG); 
-imgdaclient.src = data.imageBG;
-ctx1.clearRect(0,0,bgcanvas.width,bgcanvas.height);
-imgdaclient.onload = function() {
-ctx1.drawImage(imgdaclient, positionx, positiony);
- }          
+var objDiv = document.getElementById("testichat");
+objDiv.scrollTop = objDiv.scrollHeight;
+
+}}
 });
+	
+	
 	
  socket.on('setuproomser', function (data) {
  var imgdaclient = new Image();
@@ -193,122 +80,31 @@ imgdaclient.onload = function() {
 }
 	});	
  
-  socket.on('setupcanvasser', function (data) {
- var imgdaclient = new Image();
-imgdaclient.src = data.canvasstring; 
-imgdaclient.onload = function() {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
- ctx.drawImage(imgdaclient,0,0);          
-}
+ socket.on('chatresponse', function (data) {
+ 
+	//alert (data.testochat);
+jQuery('<div class="testochat" style="color:red">'+ data.testochat +'</div>').appendTo('#testichat');         
+var objDiv1 = document.getElementById("testichat");
+objDiv1.scrollTop = objDiv1.scrollHeight;
 	});	
  
-  socket.on('deletezoneser', function (data) {
-ctx.clearRect(data.x, data.y, data.width, data.height); 
-   });	
-
-	socket.on('moving', function (data) {
-		if(!(data.id in clients)){
-			// a new user has come online. create a cursor for them
-   cursors[data.id] = jQuery('<div class="cursor"><div class="identif">'+ data.usernamerem +'</div>').appendTo('#cursors');
-		}
-	// Move the mouse pointer
-		cursors[data.id].css({
-			'left' : data.x,
-			'top' : data.y
-		});
-			
-		// Is the user drawing?
-		if(data.drawing && data.controlpencil && clients[data.id]){
-			
-		 drawLinerem(clients[data.id].x, clients[data.id].y, data.x, data.y,data.spessremo,data.colorem);
-		}
-		
-		// Saving the current client state
-		clients[data.id] = data;
-		clients[data.id].updated = jQuery.now();
+  socket.on('chatser', function (data) {
+ 
+	//alert (data.testochat);
+jQuery('<div class="testochat" style="color:red">'+ data.testochat +'</div>').appendTo('#testichat');         
+var objDiv1 = document.getElementById("testichat");
+objDiv1.scrollTop = objDiv1.scrollHeight;
 	});
-
-	var prev = {};
-
-  	canvas.on('mousedown', function(e){
-		e.preventDefault();
-		drawing = true;
-		prev.x = e.pageX;
-		prev.y = e.pageY;
-	
-	});
-
-	$('#respondcanvas').on('mouseup', function() { 
-drawing = false;
-canvas2base64();
-});
+ 
 
 	var lastEmit = jQuery.now();
 
 //	doc.on('mousemove', function(e){
 								 
-$('#respondcanvas').on('mousemove', function(e){								 
-								 
-		if(jQuery.now() - lastEmit > 25){
-			socket.emit('mousemove',{
-				'x': e.pageX,
-				'y': e.pageY,
-				'drawing': drawing,
-				'controlpencil': controlpencil,
-			    'usernamerem' : username,
-				'spessremo' : pencilsize,
-				'room' : roomid,
-				'colorem': color
-			});
-			lastEmit = jQuery.now();
-		}
-		// Draw a line for the current user's movement, as it is
-		// not received in the socket.on('moving') event above
-		
-		if(drawing && controlpencil){
-     	drawLine(prev.x, prev.y, e.pageX, e.pageY, color);
-			prev.x = e.pageX;
-			prev.y = e.pageY;
-			
-		}
-	});
 	
 	// Remove inactive clients after 10 seconds of inactivity
-    setInterval(function(){
-        var totalOnline = 0;
-        for(var ident in clients){
-            if(jQuery.now() - clients[ident].updated > 16000){
-
-                // Last update was more than 10 seconds ago.
-                // This user has probably closed the page
-
-                cursors[ident].remove();
-                delete clients[ident];
-                delete cursors[ident];
-            }
-            else {
-			 totalOnline++;			
-        }}
-  //      jQuery('#onlineCounter').html('Users connected: '+totalOnline);
-    },16000);
-
-	function drawLine(fromx, fromy, tox, toy, color){
-		ctx.strokeStyle = color;
-	   ctx.lineWidth = pencilsize;	
-        ctx.beginPath();
-		ctx.moveTo(fromx, fromy);
-		ctx.lineTo(tox, toy);
-		ctx.stroke();
-	}
+  
 	
-	function drawLinerem(fromx, fromy, tox, toy,spessore,colorem){
-		ctx.strokeStyle = colorem;
-       ctx.lineWidth = spessore;	
-        ctx.beginPath();
-		ctx.moveTo(fromx, fromy);
-		ctx.lineTo(tox, toy);
-		ctx.stroke();
-	}
 	function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -316,16 +112,7 @@ $('#respondcanvas').on('mousemove', function(e){
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function canvas2base64()  {
-	var string_canvas = canvas[0].toDataURL();
-	if (string64data.length !=  string_canvas.length) {
-string64data = string_canvas;	
-socket.emit('base64data',{
-				'base64data' : roomid + '_' + string64data
-				   });
-console.log ('Byte sent to server: ' + string_canvas.length);
-	}	
-}
+
 
 });
 
